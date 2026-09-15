58eb75c7c0aa7ba15469a63f44f6b212a5a160c8
StillLai <lyb0330@outlook.com>
2026-09-15 11:42:27 +0800

    fix: boot-start.cmd 添加错误输出，修正 exit code 注释

--- service/boot-start.cmd
+++ service/boot-start.cmd
@@ -14,8 +14,15 @@ set "CORE_DIR=%~dp0core"
 set "EXE=%CORE_DIR%\sing-box.exe"
 set "CONFIG=%CORE_DIR%\config-!MODE!.json"
 
-if not exist "!EXE!" exit /b 1
-if not exist "!CONFIG!" exit /b 1
+if not exist "!EXE!" (
+    echo [boot-start] ERROR: sing-box.exe not found: "!EXE!" >&2
+    exit /b 1
+)
+if not exist "!CONFIG!" (
+    echo [boot-start] ERROR: config not found: "!CONFIG!" >&2
+    exit /b 1
+)
 
+REM Launch sing-box in a new console (independent of Task Scheduler Job Object)
 cmd /c start "" "!EXE!" run -c "!CONFIG!" -D "!CORE_DIR!"
-exit /b !errorlevel!
+exit /b 0
