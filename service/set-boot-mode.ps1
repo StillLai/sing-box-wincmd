4e19d7f5d8ba318b6cc045d546713f031b90bce0
StillLai <lyb0330@outlook.com>
2026-09-16 18:48:48 +0800

    chore: 删除不再使用的 set-boot-mode.ps1

--- service/set-boot-mode.ps1
+++
@@ -1,9 +0,0 @@
-# Set WinSW service boot mode
-# Usage: powershell -File set-boot-mode.ps1 -AutoXml <path> -ManualXml <path>
-param(
-    [string]$AutoXml,
-    [string]$ManualXml
-)
-
-(Get-Content -Raw $AutoXml) -replace '<startmode>[^<]*</startmode>','<startmode>automatic</startmode>' | Set-Content -LiteralPath $AutoXml -Encoding UTF8
-(Get-Content -Raw $ManualXml) -replace '<startmode>[^<]*</startmode>','<startmode>manual</startmode>' | Set-Content -LiteralPath $ManualXml -Encoding UTF8
